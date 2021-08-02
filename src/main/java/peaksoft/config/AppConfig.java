185201737c5104ce6614c2ead48a984464b0f4ca
rasulzakirov <84726364+rasulzakirov@users.noreply.github.com>
2021-08-02 20:54:41 +0600

    spring core homework

---
+++ src/main/java/peaksoft/config/AppConfig.java
@@ -0,0 +1,34 @@
+package peaksoft.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import peaksoft.model.*;
+
+@Configuration
+@ComponentScan(basePackages = "peaksoft")
+public class AppConfig {
+
+    @Bean
+    public static Island2 getIsland(Wood3 wood) {
+        return new Island2(wood);
+    }
+
+    @Bean
+
+    public static    Egg6 getEgg(Needle7 needle) {
+        return new Egg6(needle);
+
+    }
+    @Bean
+
+    public   static Needle7 getNeedle(Deth8 deth8){
+        return  new Needle7(deth8);
+    }
+    @Bean
+
+    public static Deth8 getDeth(){
+        return  new Deth8();
+    }
+}
